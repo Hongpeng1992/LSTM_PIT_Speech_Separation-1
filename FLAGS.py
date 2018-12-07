5510d54a41359a7c497f192d727858c70b78edeb
IMLHF <red_wind@foxmail.com>
2018-12-07 18:32:08 +0800

    # add speech volume norm param(input and output)

--- FLAGS.py
+++ FLAGS.py
@@ -9,7 +9,7 @@ class NNET_PARAM:
   wav_pic is oscillograph.
   wav_spec is spectrum
   '''
-  decode = 1
+  decode = 0
   '''
   @decode_show_more
   Flag indicating show  (label_wav, label_wav_spec, label_wav_pic) or not.
@@ -17,6 +17,9 @@ class NNET_PARAM:
   wav_spec is spectrum
   '''
   decode_show_more = 1
+  decode_input_norm_speaker_volume = False
+  decode_output_norm_speaker_volume = True
+  decode_speaker_tracing = True
   # Flag indicating whether to resume training from cptk.
   resume_training = 'false'
   start_epoch = 0
@@ -39,7 +42,7 @@ class NNET_PARAM:
   keep_prob = 0.8  # Keep probability for training dropout.
   max_grad_norm = 5.0  # The max gradient normalization.
   model_type = 'BLSTM'  # BLSTM or LSTM
-  LSTM_ACTIVATION = tf.tanh
+  LSTM_ACTIVATION = tf.nn.tanh
   # LSTM_ACTIVATION = tf.nn.softmax
   MASK_TYPE='IAM'# IAM: ideal_amplitude_mask; PSM: phase_sensitive_mask.
   # MASK_TYPE='PSM'
@@ -105,7 +108,7 @@ class MIXED_AISHELL_PARAM:
 
   # WAVE_NORM=True
   WAVE_NORM = False
-  LOG_NORM_MAX = 7
+  LOG_NORM_MAX = 5
   LOG_NORM_MIN = -3
   NFFT = 512
   OVERLAP = 256
